0d941a2d8bd8d6a6addfbaa41748d54dfcde9fd2
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2023-07-04 11:43:21 +0400

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,8 +1,12 @@
+k=0
+t=1
 while True:
-  k=0 t=1
-  line = input('> ')
-  if line == 'done':
-        break
-  k += line
-  t *= line
+    try:
+        line = input('> ')
+        if line == 'done':
+            break
+        k = k + int(line)
+        t = t * int(line)
+    except:
+        print('Bad data')
 print(k, t)
